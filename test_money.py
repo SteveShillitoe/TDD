a8be8c0e38f2729d5fd9696b76ef39580ced7325
Steve Shillitoe <39330132+SteveShillitoe@users.noreply.github.com>
2022-08-31 13:11:26 +0100

    Add files via upload

--- test_money.py
+++ test_money.py
@@ -1,18 +1,41 @@
 import unittest
 
-class Dollar:
-    def __init__(self, amount):
+class Money:
+    def __eq__(self, other):
+        return self.amount == other.amount and self.currency == other.currency
+
+    def __init__(self, amount, currency):
         self.amount = amount
+        self.currency = currency
 
     def times(self, multiplier):
-        return Dollar(10)
+        return Money(self.amount*multiplier, self.currency)
+
+    def divide(self, divisor):
+        return Money(self.amout/divisor, self.currency)
 
 
 class TestMoney(unittest.TestCase):
-    def testMultiplication(self):
-        fiver = Dollar(5)
-        tenner = fiver.times(2)
+    def testMultiplicationInDollars(self):
+        fiver = Money(5, "USD")
+        tenner = Money(10, "USD")
         self.assertEqual(10, tenner.amount)
+
+    def testMultiplicationInEuros(self):
+        tenEuros = Money(10, "EUR")
+        twentyEuros = tenEuros.times(2)
+        self.assertEqual(20, twentyEuros.amount)
+        self.assertEqual("EUR", twentyEuros.currency)
+
+    def testDivision(self):
+        originalMoney = Money(4002, "KRW")
+        actualMoneyAfterDivision = originalMoney.divide(4)
+        expectedMoneyAfterDivision = Money(1000.5, "KRW")
+        self.assertEqual(expectedMoneyAfterDivision.amount, 
+                         actualMoneyAfterDivison.amount)
+        self.assertEqual(expectedMoneyAfterDivision.currency, 
+                         actualMoneyAfterDivison.currency)
+
 
 if __name__ == '__main__':
     unittest.main()
